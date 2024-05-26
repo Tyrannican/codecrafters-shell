17647ed3819e40f0cb6d54e81dc3e63f86b6a0d6
Graham Keenan <graham.keenan@outlook.com>
2024-05-26 15:21:54 +0100

    unrecognised commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+use anyhow::Result;
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+pub(crate) struct Shell {
+    stdin: io::Stdin,
+    stdout: io::Stdout,
+}
+
+impl Shell {
+    pub(crate) fn new() -> Self {
+        Self {
+            stdin: io::stdin(),
+            stdout: io::stdout(),
+        }
+    }
+
+    pub(crate) fn run(&mut self) -> Result<()> {
+        write!(self.stdout, "$ ")?;
+        self.stdout.flush()?;
+
+        let mut input = String::new();
+        self.stdin.read_line(&mut input)?;
+        self.process(input)?;
+
+        Ok(())
+    }
+
+    fn process(&mut self, input: String) -> Result<()> {
+        write!(self.stdout, "{input}: command not found")?;
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let mut shell = Shell::new();
+    shell.run()
 }
