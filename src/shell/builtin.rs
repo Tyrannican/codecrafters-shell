76e7ac3f1f2d6d1d72f47eaafecddf95db732c78
Graham Keenan <graham.keenan@outlook.com>
2026-03-14 21:30:00 +0000

    fix offset in history listing

--- src/shell/builtin.rs
+++ src/shell/builtin.rs
@@ -90,7 +90,7 @@ impl ShellBuiltin {
                     Ok(CommandOutput::Stdout(output.into_bytes()))
                 } else {
                     if let Ok(numbers) = &args[0].parse::<usize>() {
-                        let idx_from = (history.len() - 1) - numbers;
+                        let idx_from = history.len() - numbers;
                         let mut output = entries[idx_from..].join("\n");
                         output.push('\n');
                         Ok(CommandOutput::Stdout(output.clone().into_bytes()))
